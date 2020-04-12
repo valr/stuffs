31054ce424caa71ec68da3f0825516e69ba7c355
Valère Monseur <valere.monseur@ymail.com>
2020-04-12 14:43:24 +0200

    rename notify_user to notify_send and add icon option in notification

--- weechat/notify_send.py
+++ weechat/notify_send.py
@@ -6,19 +6,20 @@ def handle_msg(data, buffer, date, tags, displayed, highlight, prefix, message):
     buffer_name = weechat.buffer_get_string(buffer, "short_name")
 
     if buffer_type == "private":
-        notify_user(buffer_name, message)
+        notify_send(buffer_name, message)
     elif buffer_type == "channel" and int(highlight):
-        notify_user("{}@{}:".format(prefix, buffer_name), message)
+        notify_send("{}@{}:".format(prefix, buffer_name), message)
 
     return weechat.WEECHAT_RC_OK
 
 
-def notify_user(origin, message):
+def notify_send(origin, message):
     weechat.hook_process_hashtable("notify-send",
                                    {"arg1": "-u", "arg2": "normal",
                                     "arg3": "-t", "arg4": "10000",
                                     "arg5": "-a", "arg6": "weechat",
-                                    "arg7": origin, "arg8": message},
+                                    "arg7": "-i", "arg8": "weechat",
+                                    "arg9": origin, "arg10": message},
                                    20000, "process_cb", "")
 
     return weechat.WEECHAT_RC_OK
