83c293dbed14d3a6af3fd2100fa865f40862945a
Valère Monseur <valere.monseur@ymail.com>
2020-04-13 13:54:59 +0200

    start at the right panel if none is active yet

--- sublime-text/plugin/panel_switch/panel_switch.py
+++ sublime-text/plugin/panel_switch/panel_switch.py
@@ -13,7 +13,7 @@ class PanelSwitch(sublime_plugin.WindowCommand):
 
         active_panel = self.window.active_panel()
 
-        index_panel = panels.index(active_panel) if active_panel in panels else 0
+        index_panel = panels.index(active_panel) if active_panel in panels else -1
         index_panel = (index_panel + 1) % len(panels)
 
         self.window.run_command("show_panel", {"panel": panels[index_panel]})
