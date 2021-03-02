dffff413008bb035f75977e323e75337e6565251
Valère Monseur <valere.monseur@ymail.com>
2021-03-02 23:34:48 +0100

    add script to install the plugins, settings, commands, language syntaxes, build

---
+++ sublime-text/install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -u
+
+SOURCE="/stuffs/sublime-text"
+TARGET="$HOME"/.config/sublime-text-3/Packages
+
+if [[ "$PWD" != *"$SOURCE" ]]; then
+    echo "The script must be run from within its directory."
+fi
+
+# plugins
+for PLUGIN in "CommandRun" "MarkdownToHtml" "OnEventRun" "PanelSwitch" "UrlOpen"
+do
+    rm -f "$TARGET/$PLUGIN"
+    ln -s "$PWD/plugin/$PLUGIN" "$TARGET/$PLUGIN"
+done
+
+# settings
+for SETTING in "Default (Linux).sublime-keymap" "Default.sublime-theme" \
+    "Distraction Free.sublime-settings" "LSP.sublime-settings" \
+    "Monokai.sublime-color-scheme" "Package Control.sublime-settings" \
+    "Preferences.sublime-settings" "Terminal.sublime-settings"
+do
+    rm -f "$TARGET/User/$SETTING"
+    ln -s "$PWD/settings/$SETTING" "$TARGET/User/$SETTING"
+done
+
+# commands
+for COMMAND in "Default.sublime-commands"
+do
+    rm -f "$TARGET/User/$COMMAND"
+    ln -s "$PWD/commands/$COMMAND" "$TARGET/User/$COMMAND"
+done
+
+# language syntaxes
+for SYNTAX in "Gettext.tmLanguage" "Meson.tmLanguage"
+do
+    rm -f "$TARGET/User/$SYNTAX"
+    ln -s "$PWD/syntax/$SYNTAX" "$TARGET/User/$SYNTAX"
+done
+
+# build
+for BUILD in "exec_only_failed_output_build.py"
+do
+    rm -f "$TARGET/User/$BUILD"
+    ln -s "$PWD/build/$BUILD" "$TARGET/User/$BUILD"
+done
