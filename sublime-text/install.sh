000ca023e974e2d8381f6aafb03f709415d65752
Valère Monseur <dev@vale.re>
2021-08-06 23:21:39 +0200

    set tabstop to 2

--- sublime-text/install.sh
+++ sublime-text/install.sh
@@ -6,50 +6,50 @@ SOURCE="/stuffs/sublime-text"
 TARGET="$HOME"/.config/sublime-text/Packages
 
 if [[ "$PWD" != *"$SOURCE" ]]; then
-    echo "The script must be run from within its directory."
+  echo "The script must be run from within its directory."
 fi
 
 # plugins
 for PLUGIN in "DiffText" \
-    "MarkdownToHtml" \
-    "OpenUrlPanel" \
-    "RunCommand" \
-    "RunOnEvent" \
-    "SwitchPanel"
+  "MarkdownToHtml" \
+  "OpenUrlPanel" \
+  "RunCommand" \
+  "RunOnEvent" \
+  "SwitchPanel"
 do
-    rm -f "$TARGET/$PLUGIN"
-    ln -s "$PWD/plugins/$PLUGIN" "$TARGET/$PLUGIN"
+  rm -f "$TARGET/$PLUGIN"
+  ln -s "$PWD/plugins/$PLUGIN" "$TARGET/$PLUGIN"
 done
 
 # settings
 for SETTING in "Default (Linux).sublime-keymap" \
-    "Default.sublime-commands" \
-    "Distraction Free.sublime-settings" \
-    "LSP.sublime-settings" \
-    "LSP-pylsp.sublime-settings" \
-    "Monokai.sublime-color-scheme" \
-    "PackageDev.sublime-settings" \
-    "Package Control.sublime-settings" \
-    "Preferences.sublime-settings" \
-    "SublimeLinter.sublime-settings" \
-    "Terminal.sublime-settings"
+  "Default.sublime-commands" \
+  "Distraction Free.sublime-settings" \
+  "LSP.sublime-settings" \
+  "LSP-pylsp.sublime-settings" \
+  "Monokai.sublime-color-scheme" \
+  "PackageDev.sublime-settings" \
+  "Package Control.sublime-settings" \
+  "Preferences.sublime-settings" \
+  "SublimeLinter.sublime-settings" \
+  "Terminal.sublime-settings"
 do
-    rm -f "$TARGET/User/$SETTING"
-    ln -s "$PWD/settings/$SETTING" "$TARGET/User/$SETTING"
+  rm -f "$TARGET/User/$SETTING"
+  ln -s "$PWD/settings/$SETTING" "$TARGET/User/$SETTING"
 done
 
 # language syntaxes
 for SYNTAX in "Gettext.tmLanguage" \
-    "Jinja.sublime-syntax" \
-    "Meson.tmLanguage"
+  "Jinja.sublime-syntax" \
+  "Meson.tmLanguage"
 do
-    rm -f "$TARGET/User/$SYNTAX"
-    ln -s "$PWD/syntax/$SYNTAX" "$TARGET/User/$SYNTAX"
+  rm -f "$TARGET/User/$SYNTAX"
+  ln -s "$PWD/syntax/$SYNTAX" "$TARGET/User/$SYNTAX"
 done
 
 # builds
 for BUILD in "exec_only_failed_output_build.py"
 do
-    rm -f "$TARGET/User/$BUILD"
-    ln -s "$PWD/build/$BUILD" "$TARGET/User/$BUILD"
+  rm -f "$TARGET/User/$BUILD"
+  ln -s "$PWD/build/$BUILD" "$TARGET/User/$BUILD"
 done
