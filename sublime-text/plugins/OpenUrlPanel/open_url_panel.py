3c00d60200c9f1a94de7923249184b049295dedd
Valère Monseur <dev@vale.re>
2025-12-26 14:16:52 +0100

    rename some variables and function, and add type hints

--- sublime-text/plugins/OpenUrlPanel/open_url_panel.py
+++ sublime-text/plugins/OpenUrlPanel/open_url_panel.py
@@ -1,26 +1,34 @@
+from typing import Dict, List, Optional, cast
+
 import sublime
 import sublime_plugin
+
+UrlSettings = Dict[str, List[List[str]]]
+ProjectSettings = Dict[str, UrlSettings]
+ProjectData = Dict[str, ProjectSettings]
 
 
 class OpenUrlPanelCommand(sublime_plugin.WindowCommand):
     def run(self):
         settings = sublime.load_settings("OpenUrlPanel.sublime-settings")
-        items = settings.get("url_list", [])
+        url_list = cast(List[List[str]], settings.get("url_list", []))
 
-        project_data = self.window.project_data()
+        project_data = cast(Optional[ProjectData], self.window.project_data())
         if project_data:
-            items.extend(
+            url_list.extend(
                 project_data.get("settings", {})
                 .get("open_url_panel", {})
                 .get("url_list", [])
             )
 
-        items = sorted(items, key=lambda i: i[0])
+        url_list = sorted(url_list, key=lambda x: x[0])
 
         self.window.show_quick_panel(
-            items, lambda id: self.on_done(id, items), sublime.KEEP_OPEN_ON_FOCUS_LOST
+            url_list,
+            lambda index: self.on_select(index, url_list),
+            sublime.KEEP_OPEN_ON_FOCUS_LOST,
         )
 
-    def on_done(self, id, items):
-        if id >= 0:
-            sublime.run_command("open_url", {"url": items[id][1]})
+    def on_select(self, index: int, url_list: List[List[str]]) -> None:
+        if index >= 0:
+            sublime.run_command("open_url", {"url": url_list[index][1]})
